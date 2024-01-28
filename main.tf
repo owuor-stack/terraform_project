7f0aa33b05946bf0514adc05f9a5f6c83262ffbb
Alex Owuor <52511127+owuor-stack@users.noreply.github.com>
2024-01-28 19:23:13 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file(var.credentials_file)
+  project = "daniso-app"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+project                  = "daniso-app"
+credentials_file         = "daniso-app-f64e06a7a969.json"
+"
+
